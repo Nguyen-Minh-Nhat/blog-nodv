9c968fec1aa9370f051e75c17905b264c0759ae8
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2022-11-19 22:05:50 +0700

    update

--- src/pages/component-test/index.js
+++ src/pages/component-test/index.js
@@ -1,56 +1,29 @@
-import { Avatar, Button, TextField } from '@mui/material';
+import { Button } from '@mui/material';
 import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-
-import PhotoUpload from '../../components/PhotoUpload';
-const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
 const ComponentPage = () => {
-	const {
-		register,
-		handleSubmit,
-		formState: { errors },
-	} = useForm();
-	const [upLoadData, setUploadData] = useState(null);
-
-	const onSubmit = async (data) => {
-		const formData = new FormData();
-		formData.append('username', data.username);
-		formData.append('email', data.email);
-		formData.append('bios', data.bios);
-		formData.append('avatar', upLoadData.file);
-		console.log(formData.get('email'));
-	};
-
+	const [isActive, setIsActive] = useState(true);
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<PhotoUpload onUpload={setUploadData}>
-				<Avatar src={upLoadData?.previewImg} />
-			</PhotoUpload>
-			<TextField
-				error={!!errors?.username}
-				label="username"
-				helperText={errors?.username?.message}
-				{...register('username', {
-					required: 'user name is required filed',
-					maxLength: 60,
-				})}
-			/>
-			<TextField
-				error={!!errors?.email}
-				helperText={errors?.email?.message}
-				label="email"
-				{...register('email', {
-					required: 'email is required filed',
-					pattern: {
-						value: EMAIL_REGEX,
-						message: 'invalid email address',
-					},
-				})}
-			/>
-			<TextField label="bios" {...register('bios')} />
-			<Button type="submit">Submit</Button>
-		</form>
+		<div className="container w-80 rounded bg-slate-400 shadow">
+			<ul className="items flex flex-col gap-1 p-4">
+				{[1, 2, 3].map((item) => (
+					<li
+						key={item}
+						className="item h-10 w-fit shrink-0 rounded-xl border bg-slate-50"
+					>
+						<i className="icon h-10 w-10">icon</i>
+						{isActive && <span className="w-16">item {item}</span>}
+					</li>
+				))}
+			</ul>
+
+			<Button
+				onClick={() => setIsActive((prev) => !prev)}
+				className="toggle-active"
+			>
+				Active sidebar
+			</Button>
+		</div>
 	);
 };
 
