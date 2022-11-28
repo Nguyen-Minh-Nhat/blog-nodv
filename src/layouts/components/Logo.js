c8e81d0a9f13935128c4352e16e0095dde44a2bb
KimOanh <kimoanh06082013@gmail.com>
2022-11-29 00:56:49 +0700

    u

--- src/layouts/components/Logo.js
+++ src/layouts/components/Logo.js
@@ -1,15 +1,15 @@
-import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/images/logo.png';
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import logo from "../../assets/Images/logo.png";
 
 const Logo = () => {
-	return (
-		<Link to={'/'}>
-			<Button>
-				<img src={logo} alt="logo" className="h-full w-full object-contain" />
-			</Button>
-		</Link>
-	);
+  return (
+    <Link to={"/"}>
+      <Button>
+        <img src={logo} alt="logo" className="h-full w-full object-contain" />
+      </Button>
+    </Link>
+  );
 };
 
 export default Logo;
