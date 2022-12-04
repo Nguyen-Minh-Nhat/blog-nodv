f44574cab1bcbc5a2fd0398c6258519c7242da59
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2022-12-04 17:06:49 +0700

    Update UI WhoToFollow

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,18 +1,18 @@
-import axiosClient, { axiosClientPrivate } from "./axiosClient";
+import axiosClient, { axiosClientPrivate } from './axiosClient';
 
-const url = "/users/";
+const url = '/users';
 const userApi = {
-
-export const {
 	getAllUsers: () => axiosClientPrivate.get(url),
 	getUserProfile: (email) => axiosClient.get(`${url}${email}`),
 	searchUser: (q, page = 0, limit = 10) =>
-	axiosClientPrivate.get(`${url}/search?q=${q}&page=${page}&limit=${limit}`),
+		axiosClientPrivate.get(`${url}/search?q=${q}&page=${page}&limit=${limit}`),
 	updateUserProfile: (data) => axiosClientPrivate.put(url, data),
-	followUser: (id) => axiosClientPrivate.patch(`${url}follow/${id}`, {}),
-  unFollowUser: (id) => axiosClientPrivate.patch(`${url}unfollow/${id}`),
+	followUser: (id) => axiosClientPrivate.patch(`${url}/follow/${id}`, {}),
+	unFollowUser: (id) => axiosClientPrivate.patch(`${url}/unfollow/${id}`),
 	addTopics: (topics) => axiosClientPrivate.patch(`${url}/topics`, { topics }),
 	getOwnTopics: () => axiosClientPrivate.get(`${url}/topics`),
+	getAllUnFollow: (page = 0, limit = 3) =>
+		axiosClientPrivate.get(url + '/getAllUnFollow'),
 };
 
 export const {
@@ -21,8 +21,8 @@ export const {
 	searchUser,
 	updateUserProfile,
 	followUser,
-  getAllUnFollow,
-  unFollowUser,
+	getAllUnFollow,
+	unFollowUser,
 	addTopics,
 	getOwnTopics,
 } = userApi;
--- src/components/WhoToFollow/WhoToFollow.jsx
+++ src/components/WhoToFollow/WhoToFollow.jsx
@@ -1,95 +1,97 @@
-import { useMutation, useQuery, useQueryClient } from "react-query";
-import { useSelector } from "react-redux";
-import { toast } from "react-toastify";
-import { followUser, getAllUnFollow, unFollowUser } from "../../api/userApi";
-import ButtonFollow from "../ButtonFollow/ButtonFollow";
+import { Avatar } from '@mui/material';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { followUser, getAllUnFollow, unFollowUser } from '../../api/userApi';
+import ButtonFollow from '../ButtonFollow/ButtonFollow';
 
 const WhoToFollow = () => {
-  const userId = useSelector((state) => state.user?.data?.info?.id);
-  const queryClient = useQueryClient();
-  const { data: users } = useQuery("follows", () => getAllUnFollow());
+	const userId = useSelector((state) => state.user?.data?.info?.id);
+	const queryClient = useQueryClient();
+	const { data: users } = useQuery('follows', () => getAllUnFollow());
 
-  console.log(users);
-  const updateUsers = (updatedFollower) => {
-    queryClient.setQueryData("follows", (oldData) =>
-      oldData.map((follow) => {
-        if (follow.id === updatedFollower.id) {
-          return updatedFollower;
-        }
-        return follow;
-      })
-    );
-  };
+	console.log(users);
+	const updateUsers = (updatedFollower) => {
+		queryClient.setQueryData('follows', (oldData) =>
+			oldData.map((follow) => {
+				if (follow.id === updatedFollower.id) {
+					return updatedFollower;
+				}
+				return follow;
+			})
+		);
+	};
 
-  const followUserMutation = useMutation(followUser, {
-    onSuccess: (data) => {
-      updateUsers(data);
-      console.log(queryClient.getQueryData("follows"));
-    },
-  });
+	const followUserMutation = useMutation(followUser, {
+		onSuccess: (data) => {
+			updateUsers(data);
+			console.log(queryClient.getQueryData('follows'));
+		},
+	});
 
-  const unFollowUserMutation = useMutation(unFollowUser, {
-    onSuccess: (data) => {
-      updateUsers(data);
-    },
-  });
+	const unFollowUserMutation = useMutation(unFollowUser, {
+		onSuccess: (data) => {
+			updateUsers(data);
+		},
+	});
 
-  const handleFollow = (data, isFollow) => {
-    if (isFollow) {
-      console.log(data);
-      followUserMutation.mutate(data);
-    } else {
-      unFollowUserMutation.mutate(data);
-    }
-  };
-  return (
-    <>
-      <div>
-        <h2 className="m-0 block text-base font-medium leading-5">
-          Who To Follow
-        </h2>
+	const handleFollow = (data, isFollow) => {
+		if (isFollow) {
+			console.log(data);
+			followUserMutation.mutate(data);
+		} else {
+			unFollowUserMutation.mutate(data);
+		}
+	};
+	return (
+		<>
+			<div>
+				<h2 className="m-0 block text-base font-medium leading-5">
+					Who To Follow
+				</h2>
 
-        {users &&
-          users.map((item) => (
-            <div
-              className="relative mb-5 flex w-full items-center justify-between pt-4"
-              key={item.id}
-            >
-              <div className="container-left">
-                {/* <Link to={item.path}> */}
-                <img
-                  src={item?.avatar}
-                  alt={item.username}
-                  className="t-0 absolute block h-12 w-12 rounded-full"
-                />
-                {/* </Link> */}
-                {/* <Link to={item.path}> */}
-                <div className="ml-16 mr-8 block">
-                  <h2 className="break-all text-base font-bold">
-                    {item.username}
-                  </h2>
-                  <div className="mt-1 block  break-words">
-                    <p className=" color break-all text-sm font-normal line-clamp-2">
-                      {item?.bio}
-                    </p>
-                  </div>
-                </div>
-                {/* </Link> */}
-              </div>
-              <div>
-                <ButtonFollow
-                  isFollowed={item?.followerId?.includes(userId)}
-                  onClick={(state) => {
-                    handleFollow(item.id, state);
-                  }}
-                />
-              </div>
-            </div>
-          ))}
-      </div>
-      <span className="absolute mt-5 cursor-pointer">See all</span>
-    </>
-  );
+				{users &&
+					users.map((item) => (
+						<div
+							className="relative mb-5 flex w-full items-center justify-between pt-4"
+							key={item.id}
+						>
+							<div className="container-left flex items-center">
+								<Link to={item.email}>
+									<Avatar
+										src={item?.avatar}
+										className="h-12 w-12"
+										alt={item.username}
+									/>
+								</Link>
+								<Link to={item.email}>
+									<div className="ml-4 mr-2 block">
+										<h2 className="break-all text-base font-bold">
+											{item.username}
+										</h2>
+										<div className="mt-1 block  break-words">
+											<p className=" color break-all text-sm font-normal line-clamp-2">
+												{item?.bio}
+											</p>
+										</div>
+									</div>
+								</Link>
+							</div>
+							<div>
+								<ButtonFollow
+									isFollowed={item?.followerId?.includes(userId)}
+									onClick={(state) => {
+										handleFollow(item.id, state);
+									}}
+								/>
+							</div>
+						</div>
+					))}
+			</div>
+			<span className="absolute mt-5 cursor-pointer">See all</span>
+		</>
+	);
 };
 
 export default WhoToFollow;
