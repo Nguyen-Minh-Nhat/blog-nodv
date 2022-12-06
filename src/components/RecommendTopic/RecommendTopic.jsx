458098d88bf710126729162abdb50e027a425afd
NguyenMinhNhat <72795828+Nguyen-Minh-Nhat@users.noreply.github.com>
2022-12-06 16:31:54 +0700

    update topic

--- src/components/RecommendTopic/RecommendTopic.jsx
+++ src/components/RecommendTopic/RecommendTopic.jsx
@@ -1,67 +1,26 @@
-import { Chip } from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useQuery } from 'react-query';
+import { getRecommendTopics } from '../../api/topicApi';
+import TopicList from '../Topic/TopicList';
 
 const RecommendTopic = () => {
-  return (
-    <>
-      <div className="mt-5">
-        <h2 className="m-0 mb-5 block text-base font-medium leading-5">
-          Recommended Topic
-        </h2>
-        <Link to="/datdz">
-          <Chip
-            label="C Deletable"
-            className="mr-2 mb-5 cursor-pointer p-0 py-2 px-4 font-medium"
-            // onClick={handleClick}
-          />
-        </Link>
-        <Link to="/dz">
-          <Chip
-            label="Clickable "
-            className="mr-2 mb-5 cursor-pointer p-0 py-2 px-4 font-medium
-        "
-            // onClick={handleClick}
-          />
-        </Link>
-        <Link to="/datdz">
-          <Chip
-            label="Clickable Deletable"
-            className="mr-2 mb-5 cursor-pointer p-0 py-2 px-4 font-medium"
-            // onClick={handleClick}
-          />
-        </Link>
-        <Link to="/datdz">
-          <Chip
-            label="Clickable "
-            className="mr-2 mb-5 cursor-pointer p-0 py-2 px-4 font-medium"
-            // onClick={handleClick}
-          />
-        </Link>
-        <Link to="/datdz">
-          <Chip
-            label="Clickable Deletable"
-            className="mr-2 mb-5 cursor-pointer p-0 py-2 px-4 font-medium"
-            // onClick={handleClick}
-          />
-        </Link>
-        <Link to="/datdz">
-          <Chip
-            label="Clickable Deletable"
-            className="mr-2 mb-5 cursor-pointer p-0 py-2 px-4 font-medium"
-            // onClick={handleClick}
-          />
-        </Link>
-        <Link to="/datdz">
-          <Chip
-            label="Clickablee"
-            className="mr-2 mb-5 cursor-pointer p-0 py-2 px-4 font-medium"
-            // onClick={handleClick}
-          />
-        </Link>
-      </div>
-    </>
-  );
+	const { data: topics, isSuccess } = useQuery(
+		'recommendTopics',
+		getRecommendTopics,
+		{
+			refetchOnWindowFocus: false,
+		}
+	);
+	return (
+		<>
+			<div className="mt-10">
+				<h2 className="m-0 mb-5 block text-base font-medium leading-5">
+					Recommended Topics
+				</h2>
+				{isSuccess && <TopicList topics={topics} />}
+			</div>
+		</>
+	);
 };
 
 export default RecommendTopic;
