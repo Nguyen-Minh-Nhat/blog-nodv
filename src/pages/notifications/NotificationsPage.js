6b6462c044b3d92a0fa8ec5a4a17a3e02cf4e154
Duc Nguyen <ntrungduc228@gmail.com>
2022-12-07 18:30:55 +0700

    realtime countNotifications, new notification

--- src/pages/notifications/NotificationsPage.js
+++ src/pages/notifications/NotificationsPage.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useQuery } from "react-query";
 import { getNotifications } from "../../api/notificationApi";
 import PageWithTitle from "../../components/PageWithTitle";
 import { NotificationList } from "../../features/notification";
+import { useSelector } from "react-redux";
 
 const NotificationStatus = [
   { id: 0, title: "All" },
@@ -12,6 +13,30 @@ const NotificationStatus = [
 const NotificationsPage = () => {
   const [notifications, setNotifications] = useState([]);
   const [filter, setFilter] = useState(null);
+  const socket = useSelector((state) => state.socket.data);
+  const userId = useSelector((state) => state.user?.data?.info?.id);
+
+  const handleReceiveNotificationSocket = useCallback(
+    (payload) => {
+      const newNotification = JSON.parse(payload.body);
+      setNotifications([...notifications, newNotification]);
+    },
+    [notifications]
+  );
+
+  useEffect(() => {
+    const topic = `/topic/notifications/${userId}/new`;
+    if (socket) {
+      console.log("subscribing");
+      socket.subscribe(topic, handleReceiveNotificationSocket, { id: topic });
+    }
+    return () => {
+      if (socket) {
+        console.log("unsubscribing");
+        socket.unsubscribe(topic);
+      }
+    };
+  }, [socket, userId, handleReceiveNotificationSocket]);
 
   useQuery(["notifications", filter], () => getNotifications(filter), {
     onSuccess: (data) => {
