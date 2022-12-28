14698dc41a2d2f2450185e41e74e8d208bb6182f
Vi Nguyen <khanhvi2941@gmail.com>
2022-12-28 18:13:57 +0700

    fix count notification

--- src/pages/post/PostPage.js
+++ src/pages/post/PostPage.js
@@ -1,149 +1,150 @@
-import { useEffect } from 'react';
-import { useMutation, useQuery, useQueryClient } from 'react-query';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { updatePostToBookmark } from '../../api/bookmarkApi';
-import { createNotification } from '../../api/notificationApi';
+import { useEffect } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { updatePostToBookmark } from "../../api/bookmarkApi";
+import { createNotification } from "../../api/notificationApi";
 import {
-	deletePost,
-	getPostById,
-	likePost,
-	publishPost,
-	unLikePost,
-	unpublishPost,
-} from '../../api/postApi';
-import { updateCountNotifications } from '../../api/userApi';
-import { NotificationType } from '../../config/dataType';
-import Post from '../../features/post/components/Post';
-import { updatePostByIdToBookmark } from '../../redux/slices/bookmarkSlice';
-import { setProfile } from '../../redux/slices/profileSlice';
-import { callApiCreateNotification } from '../../utils/generationNotification';
-import Main from './components/Main';
+  deletePost,
+  getPostById,
+  likePost,
+  publishPost,
+  unLikePost,
+  unpublishPost,
+} from "../../api/postApi";
+import { updateCountNotifications } from "../../api/userApi";
+import { NotificationType } from "../../config/dataType";
+import Post from "../../features/post/components/Post";
+import { updatePostByIdToBookmark } from "../../redux/slices/bookmarkSlice";
+import { setProfile } from "../../redux/slices/profileSlice";
+import { callApiCreateNotification } from "../../utils/generationNotification";
+import Main from "./components/Main";
 
 const PostPage = () => {
-	const { id } = useParams();
-	const dispatch = useDispatch();
+  const { id } = useParams();
+  const dispatch = useDispatch();
 
-	const socket = useSelector((state) => state.socket.data);
-	const postIdsBookmark = useSelector((state) => state.bookmark.postIds);
+  const socket = useSelector((state) => state.socket.data);
+  const postIdsBookmark = useSelector((state) => state.bookmark.postIds);
 
-	const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-	const post = queryClient.getQueryData(['post', id]);
-	const userId = useSelector((state) => state.user.data?.info?.id);
+  const post = queryClient.getQueryData(["post", id]);
+  const userId = useSelector((state) => state.user.data?.info?.id);
 
-	useQuery(['post', id], () => getPostById(id), {
-		onSuccess: (data) => {
-			dispatch(setProfile(data.user));
-		},
-		retry: 0,
-	});
+  useQuery(["post", id], () => getPostById(id), {
+    onSuccess: (data) => {
+      dispatch(setProfile(data.user));
+    },
+    retry: 0,
+  });
 
-	const updateLocalPost = (updatedPost) => {
-		queryClient.setQueryData(['post', id], (oldPost) => {
-			return {
-				...oldPost,
-				...updatedPost,
-			};
-		});
-	};
+  const updateLocalPost = (updatedPost) => {
+    queryClient.setQueryData(["post", id], (oldPost) => {
+      return {
+        ...oldPost,
+        ...updatedPost,
+      };
+    });
+  };
 
-	const deletePostMutation = useMutation(deletePost, {
-		onSuccess: () => {
-			toast.success('Post deleted successfully');
-		},
-	});
+  const deletePostMutation = useMutation(deletePost, {
+    onSuccess: () => {
+      toast.success("Post deleted successfully");
+    },
+  });
 
-	const publishPostMutation = useMutation(publishPost, {
-		onSuccess: (data) => {
-			updateLocalPost(data);
-			toast.success('Post was published');
-		},
-	});
+  const publishPostMutation = useMutation(publishPost, {
+    onSuccess: (data) => {
+      updateLocalPost(data);
+      toast.success("Post was published");
+    },
+  });
 
-	const unpublishPostMutation = useMutation(unpublishPost, {
-		onSuccess: (data) => {
-			updateLocalPost(data);
-			toast.success('Post was unpublished');
-		},
-	});
-	const createNotificationLikePostMutation = useMutation(createNotification, {
-		onSuccess: (data) => {
-			console.log(data);
-		},
-	});
-	const updateUserIncreaseNumOfNotification = useMutation(
-		updateCountNotifications
-	);
-	const likePostMutation = useMutation(likePost, {
-		onSuccess: (data) => {
-			queryClient.setQueryData(['post', post.id], data);
-			callApiCreateNotification(
-				data,
-				NotificationType.LIKE,
-				createNotificationLikePostMutation,
-				userId
-			);
-			const Increase = {
-				isIncrease: true,
-				userId: data.userId,
-			};
-			updateUserIncreaseNumOfNotification.mutate(Increase);
-		},
-	});
+  const unpublishPostMutation = useMutation(unpublishPost, {
+    onSuccess: (data) => {
+      updateLocalPost(data);
+      toast.success("Post was unpublished");
+    },
+  });
+  const updateUserIncreaseNumOfNotification = useMutation(
+    updateCountNotifications
+  );
+  const createNotificationLikePostMutation = useMutation(createNotification, {
+    onSuccess: (data) => {
+      const Increase = {
+        isIncrease: true,
+        userId: data.receiverId,
+      };
+      updateUserIncreaseNumOfNotification.mutate(Increase);
+    },
+  });
 
-	const unlikePostMutation = useMutation(unLikePost, {
-		onSuccess: (data) => {
-			queryClient.setQueryData(['post', post.id], data);
-		},
-	});
+  const likePostMutation = useMutation(likePost, {
+    onSuccess: (data) => {
+      console.log(data);
+      queryClient.setQueryData(["post", post.id], data);
+      callApiCreateNotification(
+        data,
+        NotificationType.LIKE,
+        createNotificationLikePostMutation,
+        userId
+      );
+    },
+  });
 
-	const updateBookmarkMutation = useMutation(updatePostToBookmark, {
-		onSuccess: (data) => {
-			// console.log("data ", data);
-			dispatch(updatePostByIdToBookmark(data));
-		},
-	});
+  const unlikePostMutation = useMutation(unLikePost, {
+    onSuccess: (data) => {
+      queryClient.setQueryData(["post", post.id], data);
+    },
+  });
 
-	const handleReceiveLikePostSocket = (payload) => {
-		// console.log(payload);
-		const { userLikeIds } = JSON.parse(payload.body);
-		updateLocalPost({ userLikeIds: userLikeIds });
-	};
+  const updateBookmarkMutation = useMutation(updatePostToBookmark, {
+    onSuccess: (data) => {
+      // console.log("data ", data);
+      dispatch(updatePostByIdToBookmark(data));
+    },
+  });
 
-	useEffect(() => {
-		const topic = `/topic/posts/${post?.id}/like`;
-		if (socket) {
-			socket.subscribe(topic, handleReceiveLikePostSocket, { id: topic });
-		}
-		return () => {
-			if (socket) {
-				console.log('unsubscribing');
-				socket.unsubscribe(topic);
-			}
-		};
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [post?.id, socket]);
+  const handleReceiveLikePostSocket = (payload) => {
+    // console.log(payload);
+    const { userLikeIds } = JSON.parse(payload.body);
+    updateLocalPost({ userLikeIds: userLikeIds });
+  };
 
-	return (
-		<div className="flex flex-col">
-			<Main>
-				{post && (
-					<Post
-						post={post}
-						isBookmarked={postIdsBookmark?.includes(post.id)}
-						onUpdateBookmark={updateBookmarkMutation.mutate}
-						onPublish={publishPostMutation.mutate}
-						onDelete={deletePostMutation.mutate}
-						onUnpublish={unpublishPostMutation.mutate}
-						onLike={likePostMutation.mutate}
-						onUnlike={unlikePostMutation.mutate}
-					/>
-				)}
-			</Main>
-		</div>
-	);
+  useEffect(() => {
+    const topic = `/topic/posts/${post?.id}/like`;
+    if (socket) {
+      socket.subscribe(topic, handleReceiveLikePostSocket, { id: topic });
+    }
+    return () => {
+      if (socket) {
+        console.log("unsubscribing");
+        socket.unsubscribe(topic);
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [post?.id, socket]);
+
+  return (
+    <div className="flex flex-col">
+      <Main>
+        {post && (
+          <Post
+            post={post}
+            isBookmarked={postIdsBookmark?.includes(post.id)}
+            onUpdateBookmark={updateBookmarkMutation.mutate}
+            onPublish={publishPostMutation.mutate}
+            onDelete={deletePostMutation.mutate}
+            onUnpublish={unpublishPostMutation.mutate}
+            onLike={likePostMutation.mutate}
+            onUnlike={unlikePostMutation.mutate}
+          />
+        )}
+      </Main>
+    </div>
+  );
 };
 
 export default PostPage;
